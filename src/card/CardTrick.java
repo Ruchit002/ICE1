14e418865581360f0ec8055277cc44dfa3b221c4
Ruchit002 <133915604+Ruchit002@users.noreply.github.com>
2023-05-27 17:39:25 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,10 +10,7 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
  */
-public class CardTrick {
     
-    public static void main(String[] args)
-    {
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
@@ -26,7 +23,81 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-    }
     
+    import java.util.Random;
+import java.util.Scanner;
+
+public class CardTrick {
+    private static final String[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+    private static final Random random = new Random();
+    private static final int HAND_SIZE = 7;
+
+    private static class Card {
+        private final String suit;
+        private final int number;
+
+        public Card(String suit, int number) {
+            this.suit = suit;
+            this.number = number;
+        }
+
+        public String getSuit() {
+            return suit;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+    }
+Card luckyCard = new Card();
+        luckyCard.setValue(1);
+        luckyCard.setSuit("Spades");
+    
+    public static void main(String[] args) {
+        Card[] hand = generateRandomHand();
+        displayHand(hand);
+
+        Card luckyCard = new Card("Spades", 7);
+
+        System.out.print("Pick a card (any card): ");
+        Scanner scanner = new Scanner(System.in);
+        String pickedCard = scanner.nextLine();
+
+        if (isCardInHand(hand, pickedCard)) {
+            System.out.println("Congratulations! You picked the lucky card.");
+        } else {
+            System.out.println("Sorry, the lucky card was not picked.");
+        }
+
+        scanner.close();
+    }
+
+    private static Card[] generateRandomHand() {
+        Card[] hand = new Card[HAND_SIZE];
+        for (int i = 0; i < HAND_SIZE; i++) {
+            String suit = suits[random.nextInt(suits.length)];
+            int number = random.nextInt(13) + 1;
+            hand[i] = new Card(suit, number);
+        }
+        return hand;
+    }
+
+    private static void displayHand(Card[] hand) {
+        System.out.println("Randomly generated hand of cards:");
+        for (Card card : hand) {
+            System.out.println(card.getNumber() + " of " + card.getSuit());
+        }
+    }
+
+    private static boolean isCardInHand(Card[] hand, String pickedCard) {
+        for (Card card : hand) {
+            if (card.getSuit().equalsIgnoreCase(pickedCard) || String.valueOf(card.getNumber()).equals(pickedCard)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 }
 
